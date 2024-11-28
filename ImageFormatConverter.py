6f9915544d7a637ae5afa848d0d2bd36abe1b817
Paul Spooner <dudecon@hotmail.com>
2024-11-27 22:49:00 -0500

    Create ImageFormatConverter.py
    for processing node descriptions

---
+++ ImageFormatConverter.py
@@ -0,0 +1,44 @@
+from os import chdir, path, rename, listdir, makedirs
+from PIL import Image
+
+dir_path = path.dirname(path.realpath(__file__))
+chdir(dir_path)
+thesefiles = listdir()
+target = ".png"
+target_size = 800 # max dimension of image, < 1 means no resize
+new = ".jpg"
+colorspace = 'RGB' # 'L' is black and white, 'RGB' is color
+
+filename = input("node name") # set to "" to not rename files
+
+
+newdirname = "Original_"+target[1:].upper()+"s"
+newdir = f"./{newdirname}/"
+tlen = len(target)
+if not newdirname in thesefiles:
+    makedirs(newdir)
+i = 0
+for f in thesefiles:
+    if f[-tlen:] == target:
+        i += 1
+        image = Image.open(f)
+        image = image.convert(colorspace)
+        if target_size >= 1:
+            cursize = (image.width, image.height)
+            maxdim = max(cursize)
+            scale = target_size/maxdim
+            if scale < 1:
+                newdim = (int(i*scale) for i in cursize)
+                image = image.resize(newdim)
+        if len(filename):
+            name = filename + str(i) + new
+        else:
+            name = f[:-tlen] + new
+        if True: #move the originals to the backup folder
+            m = newdir + f
+            rename(f, m)
+        image.save(name)
+        print("processed", f)
+    else:
+        pass
+        #print(f)
